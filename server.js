fb035e97f901604563e6e2e4bbd8297677e4328a
Manojreact <manojd@rgmobiles.com>
2020-09-13 22:34:02 +0530

    tiktok backend

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ mongoose.connect(connection_url, {
 //api endpoints
 app.get("/", (req, res) => res.status(200).send("hello"));
 
-app.get("/v1/post", (req, res) => res.status(200).send(Data));
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
 
 app.get("/v2/posts", (req, res) => {
-  Videos.find((err, data) => {
+  Videos.find({}, (err, data) => {
     if (err) {
       res.status(500).send(err);
     } else {
